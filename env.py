ea09d24eb9e06f262ca1e48a66689674a5d67636
emilalmander <emilalmander@hotmail.se>
2023-08-17 14:46:24 +0000

    deployment commit

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"]="postgres://lacvxbfn:GJOJ_7471RhNO24qR7-S_eObeIHvypL1@trumpet.db.elephantsql.com/lacvxbfn"
+os.environ["SECRET_KEY"]="my_super_secret_key"
+os.environ["CLOUDINARY_URL"] = "cloudinary://813128856459292:iUTA4-MDjuQ2dHbtPvgamJuTo1E@dybkxrb6a"
